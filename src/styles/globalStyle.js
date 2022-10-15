d3a365095dd0bc170d8544d56e77bf8b17ab240d
박겸영 <yong_drg@naver.com>
2022-10-15 14:21:50 +0900

    초기 세팅

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  
+  * {
+    box-sizing: border-box;
+    
+  }
+  body {
+    font-family: 'SF Pro Display', sans-serif;
+    color: #484848;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  body {
+    font-family: 'SF Pro Display', sans-serif
+  }
+
+  button {
+    cursor:pointer;
+  }
+`;
+export default GlobalStyle;
